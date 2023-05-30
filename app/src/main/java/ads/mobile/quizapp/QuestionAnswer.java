883baa1c0085ddcee738030da9477e605e8c6c31
Rebecca <rebecca.moreira@hotmail.com>
2023-05-30 12:16:15 -0300

    Initial commit

---
+++ app/src/main/java/ads/mobile/quizapp/QuestionAnswer.java
@@ -0,0 +1,23 @@
+package ads.mobile.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] = {
+            "Qual empresa é dona do android?",
+            "Qual dessas respostas não é uma linguagem de programação?",
+            "Onde estamos programando um aplicativo android?",
+            "Qual a rede social de fotos?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},{"Java","Kotlin","Notepad","Python"},{"Visual Studio","IntelliJ","Visual Studio Code","Android Studio"},{"Instagram","Linkedin","Twitter","Whatsapp"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Android Studio",
+            "Instagram"
+    };
+
+
+}
